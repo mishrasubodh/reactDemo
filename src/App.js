4b5f8559accb0f67770fc8a822a386511c22d639
subodh <subodh@shipgigventures.com>
2019-10-21 02:04:39 +0530

    first comit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+     <div class="person">
+      <h1>Max</h1>
+      <p>Your Age :27</p>
+     </div>
+    );
+  }
+}
+
+export default App;
